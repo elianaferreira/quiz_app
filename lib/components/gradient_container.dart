f5cb085a9a9c809079527002a6e07066b7dbba08
Eliana Ferreira <ebferreiran@gmail.com>
2023-06-25 18:03:42 -0400

    added minor change adding padding to the container

--- lib/components/gradient_container.dart
+++ lib/components/gradient_container.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/constants/dimens.dart';
 import 'package:quiz_app/constants/quiz_colors.dart';
 
 class GradientContainer extends StatelessWidget {
@@ -9,6 +10,7 @@ class GradientContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: const EdgeInsets.all(Dimens.padding),
       width: MediaQuery.of(context).size.width,
       height: MediaQuery.of(context).size.height,
       decoration: const BoxDecoration(
--- lib/constants/dimens.dart
+++ lib/constants/dimens.dart
@@ -2,6 +2,7 @@ class Dimens {
   //paddings and margings
   static const double paddingXXXL = 60;
   static const double paddingXXL = 30;
+  static const double padding = 16;
 
   //text sizes
   static const double title = 24;
